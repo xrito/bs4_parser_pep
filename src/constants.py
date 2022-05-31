f8c2ba3c39a31b42562f5a4c1724eb99cccc4323
Dmitry K <88393376+xrito@users.noreply.github.com>
2022-05-31 20:24:37 +0400

    ver 1.1

--- src/constants.py
+++ src/constants.py
@@ -2,8 +2,8 @@ from pathlib import Path
 
 MAIN_DOC_URL = 'https://docs.python.org/3/'
 PEP_DOC_URL = 'https://peps.python.org/'
-BASE_DIR = Path(__file__).parent 
-DATETIME_FORMAT  ='%Y-%m-%d_%H-%M-%S'
+BASE_DIR = Path(__file__).parent
+DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
 EXPECTED_STATUS = {
     'A': ['Active', 'Accepted'],
     'D': ['Deferred'],
